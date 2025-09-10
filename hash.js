a9d696e8b4590177474d62302aeeb1c0c21b6e92
AnietieDaniel <anietienteabasi@gmail.com>
2025-09-10 01:13:27 +0100

    Prepare for Vercel deployment

--- hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const password = "admin123"; // change to your desired admin password
-
-bcrypt.hash(password, 10).then((hash) => {
-  console.log("Hashed password:", hash);
-});
